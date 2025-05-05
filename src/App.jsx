35b6e84ed6ea9178b581cd91155111160285a80d
ishtiaqmahmood <ishtiaqmahmood45@gmail.com>
2025-05-05 12:35:51 +0600

    final

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Header from "./components/Header.jsx";
+import { useState } from "react";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+  const inputIsValid = userInput.duration >= 1;
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App;
